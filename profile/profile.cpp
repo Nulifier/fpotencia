18714bee2b6d19d43794b070a9585136b1bc5f19
Jeffrey Steward <nulifier@gmail.com>
2022-04-22 08:32:20 -0600

    Added a profile target

---
+++ profile/profile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <chrono>
+#include <Solver_NRpolar.h>
+#include <test-models.h>
+
+int main() {
+	const auto start = std::chrono::high_resolution_clock::now();
+
+	auto model = TestModels::ieee300Model();
+
+	model.compile(false);
+
+	fPotencia::NRpolarSolver solver(model);
+	solver.maxIterations = 20;
+	solver.tolerance = 1e-6;
+
+	auto result = solver.solve();
+
+	const auto end = std::chrono::high_resolution_clock::now();
+
+	std::cout << "System did " << (result != fPotencia::Solver::Result::Solved ? "not " : "") << "converge in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+}
